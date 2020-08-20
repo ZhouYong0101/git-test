c553de020963134824a9fd9119785e8025b175e1
admin <zhouyong@qq.com>
2020-08-20 09:44:29 +0800

    将工程添加到本地仓库

---
+++ git-test-project/src/com/itheima/FirstClass.java
@@ -0,0 +1,17 @@
+package com.itheima;
+
+/**
+ * @Author：周勇
+ * @Date:2020/8/20
+ * @Description:com.itheima
+ * @version:1.0
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Project");
+        System.out.println("Hello Project");
+        System.out.println("Hello Project");
+        System.out.println("Hello Project");
+        System.out.println("Hello Project");
+    }
+}
